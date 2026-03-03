86d9c3d79f3e62fbcbc5f6e37793b52ea79357b5
varshiniks2004 <varshiniks919@gmail.com>
2026-03-03 15:41:19 +0530

    type conversion

---
+++ W3/D2/P4.js
@@ -0,0 +1,34 @@
+//conditional operator
+// let age=20;
+// if(age<13){
+//     console.log("child");
+
+// }
+// else if(age<18){
+//     console.log("teenager");
+// }
+// else{
+//     console.log("adult");
+// }
+// //switch statements
+// console.log("switch statements");
+// let day="tuesday";
+
+// switch(day){
+//     case "Monday":
+//         console.log("start of the week");
+//         break;
+//     case "Wednesday":
+//         console.log("mid of the week");
+//         break;
+//     case "friday":
+//         console.log("end of the work week");
+//         break;
+//     default:
+//         console.log("some day in the week");
+//         break;
+
+// }
+//type conversion
+let n=Number("ABC");
+console.log("n:",n,"type of n:",typeof n,"isNaN",isNaN(n));

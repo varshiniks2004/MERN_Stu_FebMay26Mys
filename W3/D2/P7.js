1250ff1521916552a80a956cd798743e7d6fbd42
varshiniks2004 <varshiniks919@gmail.com>
2026-03-03 15:42:18 +0530

    arrow function

---
+++ W3/D2/P7.js
@@ -0,0 +1,16 @@
+//arrow function
+(args)=>{
+    //body of the function
+}
+//add two numbers
+const Sum=(a,b)=>{
+    return a+b;
+}
+console.log("3+5=",Sum(3,5));
+//single line return / implict return
+const square = x=>x*x;
+console.log("square of 44:",square(44));
+const sayHello=()=> console.log("hello from arrow function");
+sayHello();
+
+

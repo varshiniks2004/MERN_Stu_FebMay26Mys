3c8b8351baca9c45291905f7c7b888993144fc67
varshiniks2004 <varshiniks919@gmail.com>
2026-03-04 13:17:50 +0530

    loops

---
+++ W3/D3/P1.js
@@ -0,0 +1,20 @@
+//loops
+//for loop
+for(let i=0;i<=5;i++){
+    console.log("i",i);
+}
+//backward loop
+for(let i=5;i>0;i--){
+    console.log("i=",i);
+}
+//nested
+for(let i=1;i<3;i++){
+    for(let j=1;j<=2;j++){
+        console.log(`i=${i},j=${j}`);
+    }
+}
+
+    
+
+
+

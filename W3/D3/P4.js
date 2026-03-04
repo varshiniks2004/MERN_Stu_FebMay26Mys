db098fedd69c3dc440d287a28d96570c0b3a6568
varshiniks2004 <varshiniks919@gmail.com>
2026-03-04 13:19:12 +0530

    forward loop continue

---
+++ W3/D3/P4.js
@@ -0,0 +1,15 @@
+//forward loop
+for(let i=0;i<5;i++){
+    if(i===2){
+        console.log("i",i);
+        break;
+    }
+}
+//continue
+for(let i=0;i<5;i++){
+    if(i===2){
+        console.log("i",i);
+        continue;
+    }
+    console.log("i",i);
+}

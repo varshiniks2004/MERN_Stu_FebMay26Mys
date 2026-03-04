745ad767666bfb48ca3f7abaa10cac744cd878e2
varshiniks2004 <varshiniks919@gmail.com>
2026-03-04 13:19:59 +0530

    basics of object

---
+++ W3/D3/P6.js
@@ -0,0 +1,11 @@
+//basics of object
+let person = {
+    name: "varsh",   
+    age: 3,
+    isStudent:false
+};
+    
+        console.log("person",person);
+        console.log("name",person.name);
+        console.log("age:",person["age"]);
+

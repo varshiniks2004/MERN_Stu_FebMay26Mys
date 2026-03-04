da94a15ce2abe8655951d8875db9bb34b0aef012
varshiniks2004 <varshiniks919@gmail.com>
2026-03-04 14:37:17 +0530

    basics of object

--- W3/D3/P6.js
+++ W3/D3/P6.js
@@ -8,4 +8,17 @@ let person = {
         console.log("person",person);
         console.log("name",person.name);
         console.log("age:",person["age"]);
-
+// add a new property
+person.city="Mysore";
+console.log("person",person);
+//modify
+person.age =31;
+//delete
+delete person.isStudent;
+console.log("person",person);
+//object constructer
+const car=new Object();
+car.name="Audi";
+car.model="A4";
+car.year="2026";
+console.log("car",car);

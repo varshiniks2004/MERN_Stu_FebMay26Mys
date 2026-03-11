9d098a5391529b759a59469b8c7df302f77066c8
varshiniks2004 <varshiniks919@gmail.com>
2026-03-11 12:34:29 +0530

    storing and retriving json data

---
+++ W4/D3/p4.js
@@ -0,0 +1,22 @@
+const jsonOutput=document.getElementById("jsonOutput");
+document.getElementById("saveBtn").addEventListener("click",
+    function(){
+        const user={
+            id: 101,
+            name:"varsh",
+            role:"full stack",
+            skills:["HTML","CSS","JS"]
+        };
+        localStorage.setItem("userProfile",JSON.stringify(user));
+        jsonOutput.textContent="user object saved as string to localStorage";
+    });
+    document.getElementById("readBtn").addEventListener("click",
+    function(){
+        const up=localStorage.getItem("userProfile");
+        console.log(JSON.parse(up));
+        console.log(up);
+        jsonOutput.textContent="userProfile" + up;
+    }
+
+    );
+    

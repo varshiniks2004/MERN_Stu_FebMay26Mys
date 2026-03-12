d14a60eb15a3134161e666340cb6667a0017c6c9
varshiniks2004 <varshiniks919@gmail.com>
2026-03-12 20:40:02 +0530

    Role-Based Route Resolver Using switch + Functions

---
+++ Assignments/assign4 role based route.js
@@ -0,0 +1,28 @@
+// Role-Based Route Resolver Using switch + Functions
+const getRoute = function(role, isLoggedIn) {
+    if (!isLoggedIn) {
+        return "login";
+    }
+
+    if (role=="admin") {
+        return "admin";
+    } 
+    else if (role=="student") {
+        return "student";
+    } 
+    else if (role=="college") {
+        return "college";
+    } 
+    else if (role=="proctor") {
+        return "proctor";
+    } 
+    else {
+        return "denied";
+    }
+};
+console.log(getRoute("admin", true));    
+console.log(getRoute("student", true));   
+console.log(getRoute("college", true));  
+console.log(getRoute("proctor", true));   
+console.log(getRoute("guest", true));     
+console.log(getRoute("admin", false));   

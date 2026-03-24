1aceac80828b6f9de749760171ec767f8dfd93e2
varshiniks2004 <varshiniks919@gmail.com>
2026-03-24 17:01:22 +0530

    handling the error event in eventemitter

---
+++ W6/D2/p8.js
@@ -0,0 +1,13 @@
+//handling the error event in eventemitter
+const EventEmitter=require("events");
+const fileEmitter=new EventEmitter();
+//register an error listener
+fileEmitter.on("error",function(errorMessage){
+    console.log("emitter handler error",errorMessage);
+});
+//normal event listener:happy scenario
+fileEmitter.on("fileProcessed",function(fileName){
+    console.log("file processed sucessfully",fileName);
+});
+fileEmitter.emit("fileProcessed","report.csv");
+fileEmitter.emit("error","File processing failed.");

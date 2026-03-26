dcb27764b19d5ed48f80cd1146afe90150f2a97d
varshiniks2004 <varshiniks919@gmail.com>
2026-03-26 17:28:35 +0530

    understanding the http module

---
+++ W6/D4/p1.js
@@ -0,0 +1,5 @@
+//understanding the http module
+//importing built in http module
+const http = require("http");
+console.log("type of http.createServer:",typeof http.createServer);
+console.log("common http methods:",http.METHODS);

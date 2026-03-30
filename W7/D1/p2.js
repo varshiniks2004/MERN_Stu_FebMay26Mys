1c52eb5519b66083a6424e72835cb7994ff23b8d
varshiniks2004 <varshiniks919@gmail.com>
2026-03-30 12:06:31 +0530

    basic routing

---
+++ W7/D1/p2.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const app=express();
+app.get("/",function(req,res){
+    res.send("home route in express server")
+});
+app.get("/",function(req,res){
+    res.send("about route in express server")
+});
+app.get("/",function(req,res){
+    res.send("products route in express server")
+});
+app.listen(4000,function(){
+    console.log("express server running at http://localhost:4000");
+});

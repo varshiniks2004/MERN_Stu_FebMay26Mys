dba15bd6ddf436111032b3b11c8a313bc70b4876
varshiniks2004 <varshiniks919@gmail.com>
2026-03-30 06:25:00 +0530

    Render features dynamically

---
+++ W4/D5/portfolio-project-20260313T064645Z-3-002/portfolio-project/js/main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded",function(){
+    // Render features dynamically
+   
+    
+    initModal();
+    
+    renderSkills();
+    initContactValidation();
+    initThemeToggle();
+    searchProjects();
+    renderEducation();
+    renderExperience();
+    renderCertifications();
+    renderHackathons();
+
+    initScrollProgress();
+    initScrollSpy();
+    initBackToTop();
+    initTypingAnimation();
+    initRecentlyViewed();
+    initDynamicGreeting();
+    initGeolocation();
+})

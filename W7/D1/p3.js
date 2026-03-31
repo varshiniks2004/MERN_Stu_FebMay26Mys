f721239845bb7ec9f6f6d9e8d6d22da2a7b1dd0b
varshiniks2004 <varshiniks919@gmail.com>
2026-03-31 13:48:12 +0530

    portfolio-v3.0

--- W7/D1/p3.js
+++ W7/D1/p3.js
@@ -2,14 +2,22 @@
 const express=require("express");
 const app=express();
 app.get("/users",function(req,res){
-    res.send("returning all users");
+  
+    res.status(200).json([{message:"sucess"},
+        {id:1,name:"varsh"},
+        {id:2,name:"varshi"},
+        {id:3,name:"varshu"},
+
+    ]);
 });
+    res.send("returning all users");
+
 //to create
 app.post("/users",function(req,res){
     //res.status sets the http status code before sending the
-    res.status(201).send("user created.");
+    res.status(200).send("user created.");
 
 });
-app.listen(4000,function(){
-    console.log("express server running at http://localhost:4000");
+app.listen(4003,function(){
+    console.log("express server running at http://localhost:4003");
 });
--- W7/D1/p6.js
+++ W7/D1/p6.js
@@ -3,7 +3,7 @@ const express=require("express");
 const app =express();
 app.get("/api/status",function(req,res){
     // re.JSON automatically serailizes the object and sets json content type
-    res.JSON({
+    res.json({
         sucess:true,
         message:"express API is working"
 
@@ -12,10 +12,13 @@ app.get("/api/status",function(req,res){
 });
 app.get("/api/error",function(req,res){
     // re.JSON automatically serailizes the object and sets json content type
-    res.status(404).JSON({
+    res.status(404).json({
         sucess:false,
         message:"req resource is not found"
 
     });
 
+});
+app.listen(3000,function(){
+    console.log("express server running at http://localhost:3000");
 });

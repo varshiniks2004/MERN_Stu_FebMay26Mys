f31b9481a3a35bbd5b217a7ee218c19c3bd71ccb
varshiniks2004 <varshiniks919@gmail.com>
2026-03-31 17:11:24 +0530

    routing

--- W7/D2/p1.js
+++ W7/D2/p1.js
@@ -20,6 +20,26 @@ apiRouter.get("/orders",function(req,res){
 });
 //mounting the router under /api base path
 app.use("/api",apiRouter);
+
+//new router//
+//products router(handles/api/products)
+const productsRouter=express.Router();
+//api products
+productsRouter.post("/",(req,res)=>{
+    res.json({
+        route:"/api/products",
+    });
+});
+// api/products/:id -> delete product
+productsRouter.delete("/:id",(req,res)=>{
+    res.json({
+        route:`/api/products/${req.params.id}`,
+        message:"delete product",
+
+    });
+});
+//mount products router under /api/products
+apiRouter.use("/products",productsRouter);
 app.listen(4000,function(){
     console.log("express server running at http://localhost:4000");
 });

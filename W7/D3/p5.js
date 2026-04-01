3ab2c27bf0c9b24ca38e836bf774df7c29f42bc1
varshiniks2004 <varshiniks919@gmail.com>
2026-04-01 12:32:00 +0530

    JWT fundamenyals:token gen and verify

---
+++ W7/D3/p5.js
@@ -0,0 +1,26 @@
+//JWT fundamenyals:token gen and verify
+const jwt=require("jsonwebtoken");
+const secretKey="monkey123";
+//payload holds small non sensitive data]
+const payload={
+    userId:101,
+    role:"member"
+
+};
+const token=jwt.sign(payload,secretKey,{expiresIn:"1h"});
+console.log("token generated sucessfully\n",token);
+const tokenParts=token.split(".");
+console.log("header section:",tokenParts[0]);
+console.log("payload section:",tokenParts[1]);
+console.log("signature section:",tokenParts[2]);
+console.log("jwt part count :",tokenParts.length);
+try{
+    //jwt.verify() checks trust,signature and expiration
+    const verifiedPayload=jwt.verify(token,secretKey);
+    console.log("verified payload",verifiedPayload);
+}
+catch(error){
+    console.log("verification failed",error.message);
+}
+const decodeWithoutVerification=jwt.decode(token);
+console.log("decoded token:",decodeWithoutVerification);

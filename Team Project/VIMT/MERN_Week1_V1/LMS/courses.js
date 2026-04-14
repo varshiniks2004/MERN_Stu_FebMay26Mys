70ce64d357f4a299da805406c049035170707efd
varshiniks2004 <varshiniks919@gmail.com>
2026-04-14 12:28:02 +0530

    courses

---
+++ Team Project/VIMT/MERN_Week1_V1/LMS/courses.js
@@ -0,0 +1,43 @@
+const courses=[
+    {
+        id:1,
+        limit:150,
+        title:"Frontend developer",
+        Description:"client based development",
+        lists:[
+            {name:"HTML"},
+            {name:"CSS"},
+            {name:"JavaScript"},
+        ]
+
+    },
+    {
+        id:2,
+        limit:100,
+        title:"Backend developer",
+        Description:"client based development",
+        lists:[
+            {name:"Java"},
+            {name:"Python"},
+            {name:"MongoDb"},
+           
+        ]
+
+    },
+    {
+        id:3,
+        limit:200,
+        title:"Full stack developer",
+        Description:"client based development",
+        lists:[
+            {name:"Java"},
+            {name:"Python"},
+            {name:"MongoDb"},
+            {name:"Nodejs"},
+            {name:"Expressjs"},
+
+        ]
+
+    },
+];
+module.exports=courses;

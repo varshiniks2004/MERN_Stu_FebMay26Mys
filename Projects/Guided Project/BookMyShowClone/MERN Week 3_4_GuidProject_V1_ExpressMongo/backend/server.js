76497606d2ca4f2003c387e82a439f9522426d21
varshiniks2004 <varshiniks919@gmail.com>
2026-04-21 10:02:53 +0530

    basic app set up

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/server.js
@@ -0,0 +1,7 @@
+//load details from environment variables
+require("dotenv").config();
+const app=require("./app");
+const PORT=process.env.PORT||5000;
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+});

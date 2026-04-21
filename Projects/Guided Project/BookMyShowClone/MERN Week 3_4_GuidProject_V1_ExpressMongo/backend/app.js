303f7653547304f68442dc553031e6deec77df91
varshiniks2004 <varshiniks919@gmail.com>
2026-04-21 15:01:26 +0530

    added auth routes

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -1,10 +1,15 @@
-//
+//added auth routes
 const express = require("express");
+const authRoutes=require("./routes/auth.routes");
 const app=express();
+app.use(express.json());
+app.use("/api/auth",authRoutes);
+//base url
 app.get("/",(req,res)=>{
     res.status(200).json({
     sucess:true,
     message:"Movie booking API is running..",
     });
 });
+
 module.exports=app;

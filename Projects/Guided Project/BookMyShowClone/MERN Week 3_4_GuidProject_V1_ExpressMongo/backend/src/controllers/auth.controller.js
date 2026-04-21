c2b11a99e15d4d8cf3de173db9da477fe0dd2dd0
varshiniks2004 <varshiniks919@gmail.com>
2026-04-21 14:58:12 +0530

    controller function created with respect to auth

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/controllers/auth.controller.js
@@ -0,0 +1,47 @@
+//controller function created with respect to auth
+
+const authService = require("../services/auth.service");
+
+// Register
+exports.register = async(req,res,next)=>{
+    try {
+        const result = await authService.registerUser(req.body);
+
+        res.status(201).json({
+            success:true,
+            message:"User registered.OTP sent.",
+            data:result,
+        });
+    } catch (error) {
+        next (error);
+    }
+};
+
+// Verify OTP
+exports.verifyOTP = async(req,res,next)=>{
+    try {
+        await authService.verifyOTP(req.body);
+
+        res.status(200).json({
+            success:true,
+            message:"OTP Verified successfully",
+        });
+    } catch (error) {
+        next(error)
+    }
+};
+
+// Login 
+exports.login = async(req,res,next)=>{
+    try {
+        const result = await authService.loginUser(req.body);
+
+        res.status(200).json({success:true,
+            message:"Login successful",
+            data:result
+        });
+            
+    } catch (error) {
+        next(error);
+    }
+}

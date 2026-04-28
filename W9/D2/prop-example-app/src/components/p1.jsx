2d9f85580b8cb01e32b9c54dc918eba79a121173
varshiniks2004 <varshiniks919@gmail.com>
2026-04-28 15:30:34 +0530

    prop basics

---
+++ W9/D2/prop-example-app/src/components/p1.jsx
@@ -0,0 +1,21 @@
+// import { React } from "react";
+//Props basics
+
+//child component
+function WelcomeCard(props){
+    return(
+        <div className="card">
+            <h2>Hello,{props.name}</h2>
+            <p>Role: {props.role}</p>
+        </div>
+    )
+}
+//parent component
+export function PropBasics(){
+    return(
+        <>
+        <WelcomeCard name="varsh" role="React developer" />
+        <WelcomeCard name="Developer" role="UI/UX developer" />
+        </>
+    );
+}

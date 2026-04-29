c3897358b11eee6d8c0f024f7116d00909f1f3b5
varshiniks2004 <varshiniks919@gmail.com>
2026-04-29 10:24:32 +0530

    importing prop child and propdrillingDemo

---
+++ W9/D2/prop-example-app/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+// import { PropBasics } from "./components/P1"
+// import { PropDestructuring } from "./components/P2"
+// import { PropsChildren } from './components/P3';
+import {PropDrillingDemo} from "./components/P4"
+
+function App() {
+
+  return (
+    <>
+      {/* <PropBasics/> */}
+      {/* <PropDestructuring/> */}
+      {/* <PropsChildren /> */}
+      <PropDrillingDemo />
+    </>
+  )
+}
+
+export default App

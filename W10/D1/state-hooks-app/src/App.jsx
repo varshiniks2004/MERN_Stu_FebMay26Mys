1bacc72a74fd93d7cac4c7a3b98b094c703ad9e4
varshiniks2004 <varshiniks919@gmail.com>
2026-05-04 12:04:26 +0530

    importing event handling state

--- W10/D1/state-hooks-app/src/App.jsx
+++ W10/D1/state-hooks-app/src/App.jsx
@@ -3,14 +3,14 @@ import { useState } from 'react'
 import './App.css'
 // import { UseStateBasics } from './components/p1';
 import { UpdatingObjectsArraysState } from './components/p2';
-// import { EventHandlingState } from './components/p3';
+import { EventHandlingState } from './components/p3';
 
 function App() {
   return(
     <>
     {/* <UseStateBasics/> */}
     <UpdatingObjectsArraysState/>
-    {/* <EventHandlingState/> */}
+     <EventHandlingState/> 
     </>
   )
 }
